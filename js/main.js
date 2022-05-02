4e8157abaf31c7c9bff97bcaa72c819bc4a582ba
sajankafle26 <64882150+sajankafle26@users.noreply.github.com>
2022-05-02 21:09:04 +0545

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(window).scroll(function(){
+    if($(this).scrollTop()> 250)
+    {
+        $("header").addClass("sticky")
+    }
+    else
+    {
+        $("header").removeClass("sticky")
+    }
+})
